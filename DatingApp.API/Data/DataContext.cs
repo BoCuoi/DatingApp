31de72cdc152ec4ad31ba461de7046a63118cc3d
Nguyen Le Xuan Tung <26731234+BoCuoi@users.noreply.github.com>
2019-01-10 10:16:52 +0700

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+        //Tell entity framework about entity; Values is the Table_name in SQL
+        public DbSet<Value> Values { get; set; }
+    }
+}
