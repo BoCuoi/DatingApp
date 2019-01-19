601316d9190054d3106e43f8ce4c28093a2b95b7
Nguyen Le Xuan Tung <26731234+BoCuoi@users.noreply.github.com>
2019-01-19 11:31:44 +0700

    Almost done with AutoMapper

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
 namespace DatingApp.API.Data
 {
     public interface IDatingRepository
@@ -5,6 +9,10 @@ namespace DatingApp.API.Data
          // Using generic type
          // Add the type of T [T is User or Photo], take T entity as param
          // an constrain this method to just classes where T is type of Class
-         void Add<T>(T entity) where T: class;
+        void Add<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
     }
 }
